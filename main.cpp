9087c0047e0b35db4f0a1159505eec0a2792f5cb
Hippolyte Barraud <hippolyte.barraud@gmail.com>
2017-09-26 13:41:19 +0800

    Fixed printf on GCC

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <password_space.hpp>
+#include <cinttypes>
 
 int main() {
     using hippobaro::password_cellphone::password_space;
 
     OPTIONAL_CONSTEXPR auto possibilities = password_space<3, 3>().resolve();
 
-    printf("Result : %llu\n", possibilities);
+    printf("Result : %" PRIu64 "\n", possibilities);
     return EXIT_SUCCESS;
 }
